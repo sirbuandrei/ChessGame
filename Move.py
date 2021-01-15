d08759b4584f330df468ba01410b1fc29bf55567
sirbuandrei <60944128+sirbuandrei@users.noreply.github.com>
2021-01-15 12:27:12 +0200

    Add files via upload

--- Move.py
+++ Move.py
@@ -1,206 +1,205 @@
-# CHECK_MOVE FUNCTION WILL RETURN 2 PARAMETERS: 1st IF THE MOVE CAN BE MADE, 2nd IF WITH THAT MOVE THE PIECE CAPTURES
-# ANOTHER PIECE
-def check_move(x_from, y_from, x_to, y_to, color, piece, grid):
-    # CHECK MOVE FOR PAWN
-    # A PAWN CAN MOVE ONLY FORWARD 1 OR 2 BLOCKS AND MOVE DIAGONALLY 1 BLOCK IF IT CNA CAPTURE ANOTHER PIECE
-    if piece == 'P':
-        # IF THE COLOR IS WHITE THE MOVEMENT CAN BE ONLY ( TOWARDS THE BOTTOM OF THE BOARD )
-        if color == 'w':
-            if x_to < x_from:
-                if x_from - x_to <= 2 and y_to == y_from:
-                    if grid[x_to][y_to] == '--':
-                        return True, False
-                if x_from - x_to == 1 and abs(y_to - y_from) == 1:
-                    if grid[x_from - 1][y_from - 1][0] == 'b' or grid[x_from - 1][y_from + 1][0] == 'b':
-                        return True, True
-        # IF THE COLOR IS BLACK THE MOVEMENT CAN BE ONLY FORWARD ( TOWARDS THE TOP OF THE BOARD )
-        elif color == 'b':
-            if x_to > x_from:
-                if x_from - x_to <= 2 and y_to == y_from:
-                    if grid[x_to][y_to] == '--':
-                        return True, False
-                if x_to - x_from == 1 and abs(y_to - y_from) == 1:
-                    if grid[x_from + 1][y_from - 1][0] == 'w' or grid[x_from + 1][y_from + 1][0] == 'w':
-                        return True, True
+pieces = []
 
-    # CHECK MOVE FOR ROCK
-    # A ROCK CAN MOVE FORWARD, BACKWARD AND SIDE TO SIDE
-    if piece == 'R':
-        ok = False
-        # GET THE POSITION WHERE THE ROCK CAN BE MOVED
-        positions_rock = []
-        for x in range(x_from + 1, 8):
-            if 8 - x != 0:
-                positions_rock.append((8 - x, 0))
-        for x in range(0, x_from + 1):
-            if -(x_from - x) != 0:
-                positions_rock.append((-(x_from - x), 0))
-        for y in range(y_from + 1, 8):
-            if 8 - y != 0:
-                positions_rock.append((0, 8 - y))
-        for y in range(0, y_from + 1):
-            if -(y_from - y) != 0:
-                positions_rock.append((0, -(y_from - y)))
+grid = [['bR', 'bN', 'bB', 'bK', 'bQ', 'bB', 'bN', 'bR'],
+        ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
+        ['--', '--', '--', '--', '--', '--', '--', '--'],
+        ['--', '--', '--', '--', '--', '--', '--', '--'],
+        ['--', '--', '--', '--', '--', '--', '--', '--'],
+        ['--', '--', '--', '--', '--', '--', '--', '--'],
+        ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
+        ['wR', 'wN', 'wB', 'wK', 'wQ', 'wB', 'wN', 'wR']]
 
-        for position in positions_rock:
-            x, y = position
-            if x_from + x == x_to and y_from + y == y_to:
-                ok = True
 
-        if ok:
-            if x_from == x_to:
-                for y in range(min(y_from, y_to) + 1, max(y_from, y_to)):
-                    if grid[x_to][y] != '--':
-                        return False, False
-            elif y_from == y_to:
-                for x in range(min(x_from, x_to) + 1, max(x_from, x_to)):
-                    if grid[x][y_to] != '--':
-                        return False, False
+def check_and_pins(white_turn):
+    global x_from, y_from
+    pins = []
+    check = []
+    inCheck = False
+    if white_turn:
+        enemyColor = 'b'
+        allayColor = 'w'
+    else:
+        enemyColor = 'w'
+        allayColor = 'b'
 
-            if grid[x_to][y_to] == '--':
-                return True, False
-            elif grid[x_to][y_to] != '--' and color == 'w' and grid[x_to][y_to][0] == 'b':
-                return True, True
-            elif grid[x_to][y_to] != '--' and color == 'b' and grid[x_to][y_to][0] == 'w':
-                return True, True
+    # GET THE KING LOCATION
+    for i in range(0, 8):
+        for j in range(0, 8):
+            if grid[i][j][0] == allayColor and grid[i][j][1] == 'K':
+                x_from = i
+                y_from = j
+                break
 
-    # CHECK MOVE FOR BISHOP
-    # A BISHOP CAN MOVE DIAGONALLY
-    if piece == 'B':
-        ok = False
-        positions_bishop = []
+    # POSSIBLE POSITION WHERE THE ATTACK MIGHT COME FROM RELATIVE TO KING POSITION
+    positions_from_king = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (-1, -1), (1, -1)]
+
+    for pos in positions_from_king:
+        possiblePin = ()  # RESET POSSIBLE PINS
         for k in range(1, 8):
-            positions_bishop.append((k, k))
-            positions_bishop.append((-k, -k))
-            positions_bishop.append((k, -k))
-            positions_bishop.append((-k, k))
+            x_to = x_from + pos[0] * k
+            y_to = y_from + pos[1] * k
+            if 0 <= x_to < 8 and 0 <= y_to < 8:
+                if grid[x_to][y_to][0] == allayColor and grid[x_to][y_to][0] != 'K':
+                    if possiblePin == ():
+                        possiblePin = (x_to, y_to, pos[0], pos[1])
+                    else:  # IF ANOTHER ALLIED PIECE IS ON THE SAME DIR CAN BE A PIN WE DON T HAVE TO BOTHER,
+                        # NO CHECK CAN COME FROM THAT DIR
+                        break
+                elif grid[x_to][y_to][0] == enemyColor:
+                    piece = grid[x_to][y_to][1]
+                    index = positions_from_king.index(pos)
+                    if (0 <= index <= 3 and piece == 'R') or \
+                            (4 <= index <= 7 and piece == 'B') or \
+                            (piece == 'Q') or \
+                            (k == 1 and piece == 'P') and ((enemyColor == 'w' and 6 <= index <= 7) or (enemyColor == 'b') and 4 <= index <= 5) or \
+                            (k == 1 and piece == 'K'):
+                        if possiblePin == ():
+                            inCheck = True
+                            check.append((x_to, y_to, pos[0], pos[1]))
+                            break
+                        else:
+                            pins.append(possiblePin)
+                            break
+                    else:
+                        break
+            else:
+                break
 
-        for position in positions_bishop:
-            x, y = position
-            if x_from + x == x_to and y_from + y == y_to:
-                ok = True
+    positions_knight = [(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
 
-        if ok:
-            for k in range(1, abs(x_to - x_from)):
-                print(k)
-                if x_to > x_from and y_to > y_from:
-                    print('+ +')
-                    if grid[x_from + k][y_from + k] != '--':
-                        return False, False
-                elif x_to > x_from and y_to < y_from:
-                    print('+ -')
-                    if grid[x_from + k][y_from - k] != '--':
-                        return False, False
-                elif x_to < x_from and y_to > y_from:
-                    print('- +')
-                    if grid[x_from - k][y_from + k] != '--':
-                        return False, False
-                elif x_to < x_from and y_to < y_from:
-                    print('- -')
-                    if grid[x_from - k][y_from - k] != '--':
-                        return False, False
-            if grid[x_to][y_to] == '--':
-                return True, False
-            elif grid[x_to][y_to] != '--' and color == 'w' and grid[x_to][y_to][0] == 'b':
-                return True, True
-            elif grid[x_to][y_to] != '--' and color == 'b' and grid[x_to][y_to][0] == 'w':
-                return True, True
+    for pos in positions_knight:
+        x_to = x_from + pos[0]
+        y_to = y_from + pos[1]
+        if 0 <= x_to < 8 and 0 <= y_to < 8:
+            piece = grid[x_to][y_to]
+            if piece[0] == enemyColor and piece[1] == 'N':
+                inCheck = True
+                check.append((x_to, y_to, pos[0], pos[1]))
 
-    # CHECK MOVE FOR KING
-    # A KING CAN MOVE FORWARD, BACKWARD, SIDE TO SIDE ADN DIAGONALLY ONLY 1 BLOCK
-    if piece == 'K':
-        ok = False
+    return inCheck, pins, check
+
+
+def all_valid_moves():
+    moves = []
+    for i in range(0, 8):
+        for j in range(0, 8):
+            if grid[i][j] != '--':
+                moves.append(possible_moves(i, j, grid[i][j][0], grid[i][j][1]))
+
+    return moves
+
+
+def possible_moves(x_from, y_from, color, piece):
+    highlight = []
+
+    if piece == 'P':
+        positions_wp = [(-1, 0), (-2, 0), (-1, -1), (-1, 1)]
+        positions_bp = [(1, 0), (2, 0), (1, 1), (1, -1)]
+
+        if color == 'b':
+            for position in positions_bp:
+                x, y = position
+                if 0 <= x_from + x < 8 and 0 <= y_from + y < 8:
+                    if y == 0:
+                        if x == 2:
+                            if grid[x_from + x][y_from + y] == '--' and grid[x_from + 1][y_from] == '--':
+                                highlight.append((x_from + x, y_from + y))
+                        elif grid[x_from + x][y_from + y] == '--':
+                            highlight.append((x_from + x, y_from + y))
+                    elif grid[x_from + x][y_from + y][0] == 'w':
+                        highlight.append((x_from + x, y_from + y))
+        elif color == 'w':
+            for position in positions_wp:
+                x, y = position
+                if 0 <= x_from + x < 8 and 0 <= y_from + y < 8:
+                    if y == 0:
+                        if x == -2:
+                            if grid[x_from + x][y_from + y] == '--' and grid[x_from - 1][y_from] == '--':
+                                highlight.append((x_from + x, y_from + y))
+                        elif grid[x_from + x][y_from + y] == '--':
+                            highlight.append((x_from + x, y_from + y))
+                    else:
+                        if grid[x_from + x][y_from + y][0] == 'b':
+                            highlight.append((x_from + x, y_from + y))
+
+    elif piece == 'R':
+        rock_positions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        for pos in rock_positions:
+            for k in range(1, 8):
+                x = pos[0] * k
+                y = pos[1] * k
+                if 0 <= x_from + x < 8 and 0 <= y_from + y < 8:
+                    if grid[x_from + x][y_from + y][0] == color:
+                        break
+                    elif grid[x_from + x][y_from + y] == '--':
+                        highlight.append((x_from + x, y_from + y))
+                    else:
+                        highlight.append((x_from + x, y_from + y))
+                        break
+
+    elif piece == 'B':
+        bishop_positions = [(1, 1), (-1, 1), (-1, -1), (1, -1)]
+        for pos in bishop_positions:
+            for k in range(1, 8):
+                x = pos[0] * k
+                y = pos[1] * k
+                if 0 <= x_from + x < 8 and 0 <= y_from + y < 8:
+                    if grid[x_from + x][y_from + y][0] == color:
+                        break
+                    elif grid[x_from + x][y_from + y] == '--':
+                        highlight.append((x_from + x, y_from + y))
+                    else:
+                        highlight.append((x_from + x, y_from + y))
+                        break
+
+    elif piece == 'K':
         positions_king = [(1, 1), (-1, -1), (1, -1), (-1, 1), (0, 1), (0, -1), (1, 0), (-1, 0)]
 
         for position in positions_king:
             x, y = position
-            if x_from + x == x_to and y_from + y == y_to:
-                ok = True
+            if 0 <= x_from + x < 8 and 0 <= y_from + y < 8:
+                if grid[x_from + x][y_from + y] == '--' or grid[x_from + x][y_from + y][0] != color:
+                    highlight.append((x_from + x, y_from + y))
 
-        if ok:
-            if grid[x_to][y_to] == '--':
-                return True, False
-            elif grid[x_to][y_to] != '--' and color == 'w' and grid[x_to][y_to][0] == 'b':
-                return True, True
-            elif grid[x_to][y_to] != '--' and color == 'b' and grid[x_to][y_to][0] == 'w':
-                return True, True
-
-    # CHECK MOVE FOR KNIGHT
-    # A KNIGHT CAN MOVE IN 'L' SHAPE
-    if piece == 'N':
-        ok = False
-        # POSITION WHERE THE KNIGHT CAN MOVE FROM IT S CURRENT POSITION
+    elif piece == 'N':
         positions_knight = [(2, 1), (2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2), (-2, 1), (-2, -1)]
 
         for position in positions_knight:
             x, y = position
-            if x_from + x == x_to and y_from + y == y_to:
-                ok = True
+            if 0 <= x_from + x < 8 and 0 <= y_from + y < 8:
+                if grid[x_from + x][y_from + y] == '--' or grid[x_from + x][y_from + y][0] != color:
+                    highlight.append((x_from + x, y_from + y))
 
-        if ok:
-            if grid[x_to][y_to] == '--':
-                return True, False
-            elif grid[x_to][y_to] != '--' and color == 'w' and grid[x_to][y_to][0] == 'b':
-                return True, True
-            elif grid[x_to][y_to] != '--' and color == 'b' and grid[x_to][y_to][0] == 'w':
-                return True, True
+    elif piece == 'Q':
+        queen_positions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, 1), (-1, -1), (1, -1)]
 
-    # CHECK MOVE FOR QUEEN
-    # A QUEEN CAN MOVE FORWARD, BACKWARD, SIDE TO SIDE ADN DIAGONALLY ( A ROCKs MOVEMENT COMBINED WITH A
-    # BISHOPs MOVEMENT )
-    if piece == 'Q':
-        ok = False
-        position_queen = []
-        for k in range(1, 8):
-            position_queen.append((k, k))
-            position_queen.append((-k, -k))
-            position_queen.append((k, -k))
-            position_queen.append((-k, k))
-        for x in range(x_from + 1, 8):
-            if 8 - x != 0:
-                position_queen.append((8 - x, 0))
-        for x in range(0, x_from + 1):
-            if -(x_from - x) != 0:
-                position_queen.append((-(x_from - x), 0))
-        for y in range(y_from + 1, 8):
-            if 8 - y != 0:
-                position_queen.append((0, 8 - y))
-        for y in range(0, y_from + 1):
-            if -(y_from - y) != 0:
-                position_queen.append((0, -(y_from - y)))
+        for pos in queen_positions:
+            for k in range(1, 8):
+                x = pos[0] * k
+                y = pos[1] * k
+                if 0 <= x_from + x < 8 and 0 <= y_from + y < 8:
+                    if grid[x_from + x][y_from + y][0] == color:
+                        break
+                    elif grid[x_from + x][y_from + y] == '--':
+                        highlight.append((x_from + x, y_from + y))
+                    else:
+                        highlight.append((x_from + x, y_from + y))
+                        break
 
-        for position in position_queen:
-            x, y = position
-            if x_from + x == x_to and y_from + y == y_to:
-                ok = True
+    return highlight
 
-        if ok:
-            if x_from == x_to:
-                for y in range(min(y_from, y_to) + 1, max(y_from, y_to)):
-                    if grid[x_to][y] != '--':
-                        return False, False
-            elif y_from == y_to:
-                for x in range(min(x_from, x_to) + 1, max(x_from, x_to)):
-                    if grid[x][y_to] != '--':
-                        return False, False
-            for k in range(1, abs(x_to - x_from)):
-                if x_to > x_from and y_to > y_from:
-                    if grid[x_from + k][y_from + k] != '--':
-                        return False, False
-                elif x_to > x_from and y_to < y_from:
-                    if grid[x_from + k][y_from - k] != '--':
-                        return False, False
-                elif x_to < x_from and y_to > y_from:
-                    if grid[x_from - k][y_from + k] != '--':
-                        return False, False
-                elif x_to < x_from and y_to < y_from:
-                    if grid[x_from - k][y_from - k] != '--':
-                        return False, False
-            if grid[x_to][y_to] == '--':
-                return True, False
-            elif grid[x_to][y_to] != '--' and color == 'w' and grid[x_to][y_to][0] == 'b':
-                return True, True
-            elif grid[x_to][y_to] != '--' and color == 'b' and grid[x_to][y_to][0] == 'w':
-                return True, True
 
-    return False, False
+def divide(x, y):
+    try:
+        return x/y
+    except ZeroDivisionError:
+        return 0
+
+
+def comprehension(a, b):
+    return [x for x in a if x not in b]
+
+
+def comprehension2(a, b):
+    return [x for x in a if x in b]
